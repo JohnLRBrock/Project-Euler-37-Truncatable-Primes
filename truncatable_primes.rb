4eb7f94b001feb212bc5c9505f534d03b08c0f20
khanthulhu <johnlrbrock@gmail.com>
2018-01-23 09:15:03 -0500

    forwards and backwards truncate added

--- truncatable_primes.rb
+++ truncatable_primes.rb
@@ -23,11 +23,11 @@ end
 # calls sieve generator if sieve doesn't currently exist or is too small
 def sieve_init(n)
   return sieve_of_eratosthenes(n) unless $sieve
-  return sieve_of_eratosthenes(n * 2) if n < $sieve.length
+  return sieve_of_eratosthenes(n * 2) if n + 1 < $sieve.length
   $sieve
 end
 
-def trincatable_prime?(n)
+def truncatable_prime?(n)
   $sieve = sieve_init(n)
   prime?(n) &&
   forwards_truncate?(n) &&
@@ -35,13 +35,28 @@ def trincatable_prime?(n)
 end
 
 def prime?(n)
-  false
+  $sieve[n]
 end
 
+# removes the front digit until there's one digit left and
+# checks if the number at each step is prime.
 def forwards_truncate(n)
-  false
+  digit_array = n.digits
+  (digit_array.length - 1).times do
+    digit_array.pop
+    p digit_array
+    return false unless prime?(digit_array.join.to_i)
+  end
+  true
 end
 
+# same as above by last digit instead of first
 def backwards_truncate(n)
-  false
+    digit_array = n.digits
+  (digit_array.length - 1).times do
+    digit_array.shift
+    p digit_array
+    return false unless prime?(digit_array.join.to_i)
+  end
+  true
 end
